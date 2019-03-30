3ddc29282c59932073b19033ef48e08df9958b3e
Azurak <byron0511@outlook.com>
2019-03-30 11:30:06 +0800

    Add question sub-component

---
+++ components/question-section/quest.js
@@ -0,0 +1,16 @@
+Component({
+    data: {
+        questionType: 1,
+        poemTitle: "",
+        poemContent: "",
+        optionA:"",
+        optionB:"",
+        optionC:"",
+        optionD:"",
+        
+    },
+    methods: {
+
+    }
+
+})
