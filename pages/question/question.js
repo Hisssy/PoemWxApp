4ca950b36eabbcbbe11fef35fc68526c31a3bea9
kano233333 <1320395666@qq.com>
2019-04-02 13:11:02 +0800

    quest2

--- pages/question/question.js
+++ pages/question/question.js
@@ -1,11 +1,34 @@
+var timer;
+
 Page({
   data:{
     questionType:1,
     th:1,
     questShow:true,
-    questTypeArr:[1,2,3]
+    questTypeArr:[1,2,3],
+    counter:120
+  },
+  onLoad:function(){
+    this.setCounter();
+  },
+  setCounter:function(){
+    this.setData({
+      counter:120
+    })
+    var counter = 120;
+    timer = setInterval(function(){
+      if(counter<=0){
+        this.questShift();
+        clearTimeout(timer)
+      }
+      counter--;
+      this.setData({
+        counter:counter
+      })
+    }.bind(this),1000)
   },
   questShift:function(){
+    clearInterval(timer);
     this.setData({
       questShow:false,
       questionType:this.data.questTypeArr[this.data.th],
@@ -14,5 +37,6 @@ Page({
     this.setData({
       questShow:true
     })
+    this.setCounter();
   }
 })
