008e1ef82adb232632ee06a0a84735428fc4f2a3
kano233333 <1320395666@qq.com>
2019-04-03 14:42:19 +0800

    timer

--- pages/question/question.js
+++ pages/question/question.js
@@ -6,25 +6,25 @@ Page({
     th:1,
     questShow:true,
     questTypeArr:[1,2,3],
-    counter:120
+    counter:10
   },
   onLoad:function(){
     this.setCounter();
   },
   setCounter:function(){
     this.setData({
-      counter:120
+      counter:10
     })
-    var counter = 120;
+    var counter = 10;
     timer = setInterval(function(){
-      if(counter<=0){
+      if(counter>0){
+        counter--;
+        this.setData({
+          counter:counter
+        })
+      }else{
         this.questShift();
-        clearTimeout(timer)
       }
-      counter--;
-      this.setData({
-        counter:counter
-      })
     }.bind(this),1000)
   },
   questShift:function(){
